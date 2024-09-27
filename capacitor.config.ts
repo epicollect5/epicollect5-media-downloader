64cda411bf0df791492fe1091cb3cdefdd905ff9
Mirko <mirko.menegazzo@gmail.com>
2024-09-27 21:16:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Epicollect5 Media Downloader',
+  webDir: 'dist'
+};
+
+export default config;
